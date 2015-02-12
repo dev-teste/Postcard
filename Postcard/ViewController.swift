133972814b902747a238dcf1c6ece1e455afd3ef
André Ramos <andreramos@Andres-Air.home>
2015-02-12 12:37:40 +0000

    new label

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterMessageTextField: UITextField!
     @IBOutlet weak var mailButton: UIButton!
+    @IBOutlet weak var nameLabel: UILabel!
     
     
     
@@ -40,6 +41,11 @@ class ViewController: UIViewController {
         
         
         mailButton.setTitle("Mail Sent", forState: UIControlState.Normal);
+        
+        nameLabel.hidden = false;
+        nameLabel.text = enterNameTextField.text;
+        nameLabel.textColor = UIColor.blueColor();
+        enterNameTextField.text = "";
     }
 
 }
